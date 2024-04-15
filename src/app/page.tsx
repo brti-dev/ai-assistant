27a44fc38aa342e3a192e4affc92e514e42ea26f
Matt Berti <mat.berti@gmail.com>
2024-04-16 00:14:06 +0700

    home page (redirect)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import ChatBot from '@/components/chat-bot'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
+  redirect('/strategic-planning')
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello world
+    <main className="flex flex-col items-center justify-between p-24">
+      <b className="my-10">Hello world</b>
+      <ul>
+        <li>
+          <Link href="/strategic-planning" className="underline">
+            Strategic Planning Instructor
+          </Link>
+        </li>
+      </ul>
     </main>
   )
 }
